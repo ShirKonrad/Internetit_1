529a3325c13c3679be9e9c485c46eaeabb15403e
shir konrad <shir.konrad@gmail.com>
2022-11-19 17:32:21 +0200

    make it responsive

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,78 +1,77 @@
-
 import { useState } from "react";
 import FoodOption from "./components/FoodOption";
-import "./App.css"
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
+import "./App.css";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
 
 function App() {
   const foods = [
     {
-      img: '/assets/apple.jpg',
-      title: 'apple',
+      img: "/assets/apple.jpg",
+      title: "apple",
       price: 6,
-      second_img: '/assets/apple.jpg',
-      shop: 'super1',
-      description: 'Red apple'
+      second_img: "/assets/apple.jpg",
+      shop: "super1",
+      description: "Red apple",
     },
     {
-      img: '/assets/banana.jpg',
-      title: 'bannana',
+      img: "/assets/banana.jpg",
+      title: "bannana",
       price: 5,
-      second_img: '/assets/banana.jpg',
-      shop: 'super2',
-      description: 'Yellow banana'
+      second_img: "/assets/banana.jpg",
+      shop: "super2",
+      description: "Yellow banana",
     },
     {
-      img: '/assets/watermelon.jpg',
-      title: 'watermelon',
+      img: "/assets/watermelon.jpg",
+      title: "watermelon",
       price: 24,
-      second_img: '/assets/watermelon.jpg',
-      shop: 'super3',
-      description: 'Green and red watermelon'
+      second_img: "/assets/watermelon.jpg",
+      shop: "super3",
+      description: "Green and red watermelon",
     },
     {
-      img: '/assets/orange.jpg',
-      title: 'orange',
+      img: "/assets/orange.jpg",
+      title: "orange",
       price: 2,
-      second_img: '/assets/orange.jpg',
-      shop: 'super1',
-      description: 'Orange orange'
+      second_img: "/assets/orange.jpg",
+      shop: "super1",
+      description: "Orange orange",
     },
 
     {
-      img: '/assets/apple.jpg',
-      title: 'apple',
+      img: "/assets/apple.jpg",
+      title: "apple",
       price: 6,
-      second_img: '/assets/apple.jpg',
-      shop: 'super5',
-      description: 'Red apple'
+      second_img: "/assets/apple.jpg",
+      shop: "super5",
+      description: "Red apple",
     },
     {
-      img: '/assets/banana.jpg',
-      title: 'bannana',
+      img: "/assets/banana.jpg",
+      title: "bannana",
       price: 5,
-      second_img: '/assets/banana.jpg',
-      shop: 'super1',
-      description: 'Yellow banana'
+      second_img: "/assets/banana.jpg",
+      shop: "super1",
+      description: "Yellow banana",
     },
     {
-      img: '/assets/watermelon.jpg',
-      title: 'watermelon',
+      img: "/assets/watermelon.jpg",
+      title: "watermelon",
       price: 24,
-      second_img: '/assets/watermelon.jpg',
-      shop: 'super3',
-      description: 'Green and red watermelon'
+      second_img: "/assets/watermelon.jpg",
+      shop: "super3",
+      description: "Green and red watermelon",
     },
     {
-      img: '/assets/orange.jpg',
-      title: 'orange',
+      img: "/assets/orange.jpg",
+      title: "orange",
       price: 2,
-      second_img: '/assets/orange.jpg',
-      shop: 'super7',
-      description: 'Orange orange'
+      second_img: "/assets/orange.jpg",
+      shop: "super7",
+      description: "Orange orange",
     },
   ];
 
@@ -81,15 +80,13 @@ function App() {
 
   const handleClose = () => {
     setIsOpen(false);
-  }
+  };
 
   return (
     <div className="app-list">
-      <h2>
-        Food Options
-      </h2>
-      {
-        foods.map((food, i) => {
+      <h2>Food Options</h2>
+      <div className="list-container">
+        {foods.map((food, i) => {
           return (
             <FoodOption
               key={i}
@@ -99,9 +96,11 @@ function App() {
               onClick={() => {
                 setSelected(food);
                 setIsOpen(true);
-              }} />)
-        })
-      }
+              }}
+            />
+          );
+        })}
+      </div>
       <Dialog
         open={isOpen}
         onClose={handleClose}
@@ -109,7 +108,7 @@ function App() {
         aria-describedby="alert-dialog-description"
       >
         <DialogTitle>
-          {`More details on ${selected ? selected.title : 'food'}`}
+          {`More details on ${selected ? selected.title : "food"}`}
         </DialogTitle>
         <DialogContent>
           <p>Where to buy: {selected && selected.shop}</p>
--- my-app/src/components/FoodOption.js
+++ my-app/src/components/FoodOption.js
@@ -1,17 +1,15 @@
-
-import './FoodOption.css'
+import "./FoodOption.css";
 
 function FoodOption(props) {
-
-    return (
-        <div className='food-item' onClick={props.onClick}>
-            <img className='food-item__img' src={props.img} />
-            <div className='food-item__details'>
-                <h2 className='food-item__title'>Name: {props.title}</h2>
-                <h2 className='food-item__price'>Price: {props.price} $</h2>
-            </div>
-        </div>
-    );
+  return (
+    <div className="food-item" onClick={props.onClick}>
+      <img className="food-item__img" src={props.img} />
+      <div className="food-item__details">
+        <h2 className="food-item__title">Name: {props.title}</h2>
+        <h2 className="food-item__price">Price: {props.price} $</h2>
+      </div>
+    </div>
+  );
 }
 
 export default FoodOption;
